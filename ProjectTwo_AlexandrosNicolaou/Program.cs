627ecf5fa3549cbbaf1ad3c39a7c60ca54c0b1f6
alnicolaou96 <alnicolaou96@gmail.com>
2018-05-25 10:22:38 -0400

    Starting and Ending Numbers

--- ProjectTwo_AlexandrosNicolaou/Program.cs
+++ ProjectTwo_AlexandrosNicolaou/Program.cs
@@ -10,6 +10,13 @@ namespace ProjectTwo_AlexandrosNicolaou
     {
         static void Main(string[] args)
         {
+
+            //Starting and ending numbers
+            Console.WriteLine("input your starting number");
+            int min = int.Parse(Console.ReadLine());
+            Console.WriteLine("input your ending number ");
+            int max = int.Parse(Console.ReadLine());
+
         }
     }
 }
