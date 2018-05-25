1d80a93d351c7790cf360949fc91f438ba64b45a
alnicolaou96 <alnicolaou96@gmail.com>
2018-05-25 11:19:38 -0400

    input 6 lucky numbers

--- ProjectTwo_AlexandrosNicolaou/Program.cs
+++ ProjectTwo_AlexandrosNicolaou/Program.cs
@@ -17,6 +17,18 @@ namespace ProjectTwo_AlexandrosNicolaou
             Console.WriteLine("input your ending number ");
             int max = int.Parse(Console.ReadLine());
 
+            //guess 6 number array
+            Console.WriteLine("Guess six lucky numbers");
+            int[] luckyNumber = new int[6];
+            for(int i=0 ; i < luckyNumber.Length; i++)
+            {
+                luckyNumber[i] = int.Parse(Console.ReadLine());
+                while (luckyNumber[i]<min||luckyNumber[i]>max)
+                {
+                    Console.WriteLine("number is invalid \ninput a number between {0} and {1}",min,max);
+                    luckyNumber[i] = int.Parse(Console.ReadLine());
+                }            
+            }
         }
     }
 }
